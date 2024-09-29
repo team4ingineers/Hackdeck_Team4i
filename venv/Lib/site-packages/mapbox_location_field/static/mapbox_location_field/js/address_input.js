8e3e3e8e80de0916fc151ad3236c7b5622305cb2
Vinish Rexson <vinishrexson2005@gmail.com>
2024-09-29 18:15:13 +0530

    all done i think

--- venv/Lib/site-packages/mapbox_location_field/static/mapbox_location_field/js/address_input.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  $(document).on("reverse-geocode", function (e, map_id, input) {
-    x = $("#" + map_id + ".js-mapbox-address-input-location-field");
-    x.val(input);
-  });
-});
